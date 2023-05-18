39f06cb7fcb67f3e8c1b99b0ae7d685ded478dfd
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-18 15:45:39 +0900

    entity 설정완료

---
+++ src/main/java/com/gyojincompany/board/JpaBoardProject20230518Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardProject20230518Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardProject20230518Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/board/JpaBoardProject20230518ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardProject20230518ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
