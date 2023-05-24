57aabee36989667597603a2dd4b024765a77c59d
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-24 09:56:30 +0900

    질문 내용 보기 추가

--- src/main/java/com/gyojincompany/board/service/QuestionService.java
+++ src/main/java/com/gyojincompany/board/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.gyojincompany.board.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gyojincompany.board.entity.Question;
+import com.gyojincompany.board.exception.DataNotFoundException;
 import com.gyojincompany.board.repository.QuestionRepository;
 
 @Service
@@ -17,6 +19,20 @@ public class QuestionService {
 	public List<Question> getQuestionList() {
 		List<Question> questionList = questionRepository.findAll();
 		return questionList;
+	}
+	
+	public Question getQuestion(Integer id) {
+		
+		Optional<Question> optQuestion = questionRepository.findById(id);//1개 반환되거나 없거나
+		
+		if(optQuestion.isPresent()) {
+			Question question = optQuestion.get();
+			return question;
+		} else {
+			throw new DataNotFoundException("선택하신 질문은 없는 글입니다.");
+		}
+		
+		
 	}
 
 }
